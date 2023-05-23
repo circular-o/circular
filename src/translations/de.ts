339bd5e3082e6c04a3c4d47bf05e2f7bd84b0a6a
Jeyson Buitrago <jeysonj2@gmail.com>
2023-05-23 18:52:36 +0200

    Revert "Update de.ts (#1339)"
    This reverts commit 02bdd8206e301d77c5c0fa97c0fb53811d58f1ad.

--- src/translations/de.ts
+++ src/translations/de.ts
@@ -28,7 +28,7 @@ const translation: Translation = {
   scrollToStart: 'Zum Anfang scrollen',
   selectAColorFromTheScreen: 'Wähle eine Farbe vom Bildschirm',
   showPassword: 'Passwort anzeigen',
-  slideNum: slide => `Folie ${slide}`,
+  slideNum: slide => `Gleiten ${slide}`,
   toggleColorFormat: 'Farbformat umschalten'
 };
 
