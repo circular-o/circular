3881538520ecdc8f07a41c3b5e9e6a8a87aebe80
Jeyson Buitrago <jeysonj2@gmail.com>
2023-07-10 14:45:23 +0200

    fix(icon): Fixes icon color

--- src/components/icon/library.material.ts
+++ src/components/icon/library.material.ts
@@ -3,7 +3,8 @@ import type { IconLibrary } from './library';
 
 const library: IconLibrary = {
   name: 'material',
-  resolver: name => getBasePath(`assets/icons/material/${name}.svg`)
+  resolver: name => getBasePath(`assets/icons/material/${name}.svg`),
+  mutator: svg => svg.setAttribute('fill', 'currentColor')
 };
 
 export default library;
